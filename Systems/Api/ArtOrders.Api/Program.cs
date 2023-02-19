493f499f19ece14f5f68c0cd22c7dd6bd3934298
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-02-19 22:24:31 +0300

    Добавлен Logger

--- Systems/Api/ArtOrders.Api/Program.cs
+++ Systems/Api/ArtOrders.Api/Program.cs
@@ -1,6 +1,10 @@
+using ArtOrders.Api.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.AddAppLogger();
 
 builder.Services.AddControllers();
 
