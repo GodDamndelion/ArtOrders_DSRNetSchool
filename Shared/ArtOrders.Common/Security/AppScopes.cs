431a5ae3e07850e95d2729fbc7ef699af532eba7
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-02-19 21:30:13 +0300

    Самое начало

---
+++ Shared/ArtOrders.Common/Security/AppScopes.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtOrders.Common.Security;
+
+// TODO: Поменять тут названия разрешений
+
+public static class AppScopes
+{
+    public const string BooksRead = "books_read";
+    public const string BooksWrite = "books_write";
+}
