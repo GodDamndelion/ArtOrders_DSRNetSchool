048346849f926655727296ba1e691185f0664403
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-02-21 12:02:59 +0300

    Добавлен HealthCheck

---
+++ Systems/Api/ArtOrders.Api/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace ArtOrders.Api.Configuration;
+
+using ArtOrders.Common;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+public static class HealthCheckConfiguration
+{
+    public static IServiceCollection AddAppHealthChecks(this IServiceCollection services)
+    {
+        services.AddHealthChecks()
+            .AddCheck<SelfHealthCheck>("ArtOrders.API");
+
+        return services;
+    }
+
+    public static void UseAppHealthChecks(this WebApplication app)
+    {
+        app.MapHealthChecks("/health"); // Обычный health не должен ни от чего зависеть (От БД и т.п.)
+
+        app.MapHealthChecks("/health/detail", new HealthCheckOptions
+        {
+            ResponseWriter = HealthCheckHelper.WriteHealthCheckResponse,
+            AllowCachingResponses = false,
+        });
+    }
+}
