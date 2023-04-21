6d056dc1637ad337c1b81b56760e2849ffd5aca4
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2023-04-21 19:24:40 +0300

    Добавлен ConfigurationService и тёмная тема

--- Systems/Web/ArtOrders.Web/Program.cs
+++ Systems/Web/ArtOrders.Web/Program.cs
@@ -1,4 +1,5 @@
 using ArtOrders.Web;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,10 +8,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 
 await builder.Build().RunAsync();
